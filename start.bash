4146db101ca7255a32b86c616c7f0613938fdd66
Leko <rexx0520@gmail.com>
2020-10-06 12:29:23 +0800

    fix: typo

--- start.bash
+++ start.bash
@@ -13,5 +13,5 @@ CHANNEL_IDS=(
 mkdir -p /tmp/live-dl
 
 for key in "${!CHANNEL_IDS[@]}"; do
-    /usr/bin/nohup /bin/bash /root/live-dl/live-dl "https://www.youtube.com/channel/${CHANNEL_IDS[${key}]}" $> "/tmp/live-dl/${key}.log" &
+    /usr/bin/nohup /bin/bash /root/live-dl/live-dl "https://www.youtube.com/channel/${CHANNEL_IDS[${key}]}" &> "/tmp/live-dl/${key}.log" &
 done
