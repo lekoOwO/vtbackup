b298bf84ee15d13ef09fd9fd6a78c1544a94ddef
leko <rexx0520@gmail.com>
2020-10-07 01:15:06 +0800

    fix: atomicparsley dependency

--- init.sh
+++ init.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 apk add --no-cache aria2 bash ffmpeg python3 perl build-base curl jq git nano exiv2-dev coreutils
 apk add exiv2 --no-cache --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
+apk add --no-cache atomicparsley --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing
 pip3 install streamlink youtube-dl yq
