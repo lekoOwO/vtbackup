729bfaac57f3c344fdd966e8f2b43e10f2dc8ae8
leko <rexx0520@gmail.com>
2020-10-19 19:01:39 +0800

    feat: add_channel.sh append empty line before new contents

--- add_channel.sh
+++ add_channel.sh
@@ -10,6 +10,7 @@ TELEGRAM_ID="${prompt_telegram_id:-${DEFAULT_TELEGRAM_CHANNEL_ID}}"
 sed -i "/CHANNEL_IDS=(/a \\ \\ [\"$name\"]=\"$channel_id\"" /root/start.bash
 
 cat >> /root/live-dl/config.yml <<EOL
+
   - name: ${name}
     youtube: https://www.youtube.com/channel/${channel_id}
     telegram: ${TELEGRAM_ID}
