49f29cbfa230819ffa9c576139cf1c58e082f400
Leko <leko@leko.moe>
2021-08-24 01:54:01 +0800

    feat: replace -gen script with pm2

--- scripts/vtbackup.init.sh
+++ scripts/vtbackup.init.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-apk add --no-cache aria2 bash ffmpeg python3 py3-pip perl build-base curl jq git nano exiv2-dev coreutils grep
+apk add --no-cache aria2 bash ffmpeg python3 py3-pip perl build-base curl jq git nano exiv2-dev coreutils grep nodejs npm
 apk add exiv2 --no-cache --repository=http://dl-cdn.alpinelinux.org/alpine/edge/community
 apk add --no-cache atomicparsley --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing
 pip3 install yq
+npm i -g pm2
